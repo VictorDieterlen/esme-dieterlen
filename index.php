544d953d165e1c31e38da9084ee2f8af3285ceaa
Victor Dieterlen <vdieterlen@csumb.edu>
2018-11-04 22:07:24 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8"/>
+    <title>Dieterlen ESME 2Z2</title>
+</head>
+<body style="max-width: 80%;
+    margin: auto;">
+
+                    <a href="/TP1 HTML/Exercice1.html">Exercice1</a><br />
+                    <a href="/TP1 HTML/Exercice2.html">Exercice2</a><br />
+                    <a href="/TP1 HTML/Exercice3.html">Exercice3</a><br />
+                    <a href="/TP1 HTML/Exercice4.html">Exercice4</a><br />
+                    <a href="/TP1 HTML/Exercice5.html">Exercice5</a><br />
+                    <a href="/TP1 HTML/Exercice6.html">Exercice6</a><br />
+                    <a href="/Mini Projet/index.html">MiniProjet</a><br />
+    
+    <h2>Mohamed Henni</h2>
+<div style="max-width: 50%;
+    margin: auto;">
+    <h3>Un grand supporter marseillais</h3></br>
+    <img src="nuuul.jpg" alt="Momo" width="300" height="333">
+</div>
+</br></br><hr></br></br>
+<div style="max-width: 50%;
+    margin: auto;">
+    <h3>Les snaps cultes</h3></br>
+    <img src="okdaccord.jpg" alt="Momo" width="300" height="333">
+</div>
+</br></br><hr></br></br>
+<div style="max-width: 50%;
+    margin: auto;">
+    <h3>Les phrases cultes</h3></br>
+    <img src="tronul.jpg" alt="Momo" width="500" height="333">
+</div>
+
+<ul type=square>
+    <li>lundi</li>
+    <li>Mardi</li>
+    <li>Mercredi</li>
+</ul>
+
+<ol start=5>
+    <li>Janvier</li>
+    <li>Février</li>
+    <li>Mars</li>
+</ol>
+
+<dl>
+    <dt>Boissons chaudes</dt>
+        <dd>Café</dd>
+        <dd>Chocolat</dd>
+        <dd>Thé</dd>
+    <dt>Boissons froides</dt>
+        <dd>Soda</dd>
+        <dd>Jus</dd>
+        <dd>Sirop</dd>
+</dl>
+
+<h1>C'est l'exercice 1</h1>
+<h2>Je suis une entête de niveau 2</h2>
+<p>Je suis un paragraphe</p>
+<h1>Je suis une entête</h1>
+<p>Et un autr paragraphe</p>
+
+<h1>Exercice 3</h1></br>
+<h2>Une liste non-ordonnée :</h2>
+
+<ul style="list-style-type:disc">
+  <li>Café</li>
+  <li>Thé</li>
+  <li>Lait</li>
+</ul>  
+<h2>Une liste ordonnée :</h2>
+
+<ol>
+  <li>Café</li>
+  <li>Thé</li>
+  <li>Lait</li>
+</ol>  
+<dl>
+    <h2>Une liste dans une liste :</h2>
+        <ul>
+  <li>Café</li>
+  <li>Thé
+    <ol type="A">
+        <li>Thé noir</li>
+        <li>Thé vert</li>
+        <ol type="i">
+          <li>Chine</li>
+          <li>Afrique</li>
+        </ol>
+    </ol>
+  </li>
+  <li>Lait</li>
+</ul></br>
+<p><a href = "http://www.google.fr">Le site Google</a></p></br>
+
+<h1>TAG Index</h1>
+<h2>Anchor example</h2>
+<h3><a name="menu">Menu</a></h3>
+<ul>
+    <li><a href="#a001">Jump to a001</a></li>
+    <li><a href="#a002">Jump to a002</a></li>
+    <li><a href="#a003">Jump to a003</a></li>
+</ul>
+<h3><a name="a001">a001</a></h3>
+<p>paragraph text ...</p>
+<h3><a name="a002">a002</a></h3>
+<p>paragraph text ...</p>
+<h3><a name="a003">a003</a></h3>
+<p>paragraph text ...</p></br>
+<p><a href = "#menu">Jump to Menu</a></p></br></br></br></br></br></br>
+
+<p><img src="nuuul.jpg" align="top">
+Aligns to the top
+The text aligns to the top of the image.</p>
+<p><img src="nuuul.jpg" alt="Example" align="middle">
+Aligns to the middle
+The text aligns to the middle of the image.</p>
+<p><img src="nuuul.jpg" alt="Example" align="bottom">
+Aligns to the bottom (Default)
+The text aligns to the bottom of the image.</p>
+<p><img src="nuuul.jpg" alt="Example" align="left">
+Aligns to the left
+The image floats to the left
+The text wrap the right of the image.
+<br clear="left">Clears the left</p>
+<p><img src="nuuul.jpg" alt="Example" align="right">
+Aligns to the right
+The image floats to the right
+The text wrap the left of the image.
+<br clear="left">Clears the right</p>
+
+<p><img src="nuuul.jpg" alt="Example" align="left" vspace="30">
+some text... </br>some text... </br> some text... </br>some text... </br> some text... </br>some text... </br>some text... </br>some text... </br>some text... </br>some text... </br>
+</p>
+<p><img src="nuuul.jpg" alt="Example" align="left" hspace="30">
+some text... </br>some text... </br>some text... </br>some text... </br>some text... </br>some text... </br>some text... </br>some text... </br>some text... </br>some text... </br>
+</p>
+
+</br></br></br></br></br></br></br></br></br></br></br>
+<table border="1">
+    <th colspan="3">Liste d'étudiants </th>
+    <tr><td><b>Prénom</b></td><td><b>Nom</b></td><td><b>Cursus</b></td></tr>
+    <tr><td>Polo</td><td>Ralph Lauren</td><td>PAM</td></tr>
+    <tr><td>Polo</td><td>Ralph Lauren</td><td>PAM</td></tr>
+    <tr><td>Polo</td><td>Ralph Lauren</td><td>PAM</td></tr>
+    <tr><td>Polo</td><td>Ralph Lauren</td><td>PAM</td></tr>
+    <tr><td>Polo</td><td>Ralph Lauren</td><td>PAM</td></tr>
+    <tr><td>Polo</td><td>Ralph Lauren</td><td>PAM</td></tr>
+    <tr><td>Polo</td><td>Ralph Lauren</td><td>PAM</td></tr>
+    <tr><td>Polo</td><td>Ralph Lauren</td><td>PAM</td></tr>
+</table>
+</br></br></br></br></br></br></br></br></br></br></br>
+<form action="/action_page.php">
+  First name:<br>
+  <input type="text" name="firstname" value="Frirst name">
+  <br>
+  Last name:<br>
+  <input type="text" name="lastname" value="Last name">
+  <br>
+  Comments:<br>
+  <textarea cols="50" rows="5" name ="Comment"></textarea>
+  <br><br>
+  <input type="submit" value="Submit">
+</form> 
+</br></br></br></br></br></br>
+<p>Choisis ta voiture</p>
+<form action="/action_page.php">
+  <select name="cars">
+    <option value="volvo">Volvo</option>
+    <option value="saab">Saab</option>
+    <option value="fiat" selected>Fiat</option>
+    <option value="audi">Audi</option>
+  </select>
+  <br><br>
+  <input type="submit">
+</form>
+
+
+<video width="320" height="240" controls>
+  <source src="https://www.w3schools.com/html/movie.mp4" type="video/mp4">
+  Your browser does not support the video tag.
+</video>
+<br><br><br><br><br>
+</body>
+<footer>
+    <hr>
+    <address>
+        3 Boulevard Michelet, 13008 Marseille
+    </address>
+</footer>
+</html>
